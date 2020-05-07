d83988ed211e4d48ec95dbe699b5574c31c42cfe
keeran2488 <49069135+keeran2488@users.noreply.github.com>
2020-05-08 00:55:18 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("my first app"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello Ninjas!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("click!"),
+      ),
+    ),
+  ));
+}
+
