27f2d7566fa8e4fa4ad683c59bccc0e3179293e2
keeran2488 <49069135+keeran2488@users.noreply.github.com>
2020-05-11 23:08:58 +0545

    added custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("my first app"),
         centerTitle: true,
+        backgroundColor: Colors.blue[700],
       ),
       body: Center(
-        child: Text("Hello Ninjas!"),
+        child: Text(
+          "Hello Ninjas!",
+          style: TextStyle(
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'Livvic'
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Text("click!"),
+        backgroundColor: Colors.blue[600],
       ),
-    ),
-  ));
+    );
+  }
 }
+
 
